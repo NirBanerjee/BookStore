9dab2a076dcba481f82c3aef2999b4f254c0aa02
Nirmoho Banerjee <nban12345@gmail.com>
2017-11-29 03:46:18 -0500

    Implemented Static Files and Compressors

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -37,11 +37,12 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'store',
     'registration',
     'social.apps.django_app.default',
     'bootstrap3',
     'bootstrap_themes',
+    'compressor',
+    'store',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -112,7 +113,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')     
+COMPRESS_ENABLED = True                           
+STATICFILES_FINDERS = (
+        'django.contrib.staticfiles.finders.FileSystemFinder',
+        'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+        'compressor.finders.CompressorFinder',
+)   
 #Registration
 ACCOUNT_ACTIVATION_DAYS = 7
 REGISTRATION_AUTO_LOGIN = True
